d769adcce9e85987aa4356888943d295e11187ee
Calcutator <114863662+Calcutatator@users.noreply.github.com>
2025-10-28 13:25:46 +0100

    Update config.ts

--- sync/trigger/chains/evm/abstract/bigquery/config.ts
+++ sync/trigger/chains/evm/abstract/bigquery/config.ts
@@ -1,1 +1,11 @@
+import { FACILITATORS_BY_CHAIN } from '@facilitators/config';
+import { createEvmChainConfig } from '@/trigger/fetch/bitquery/query';
+import { Chain } from '@/trigger/types';
 
+export const baseChainConfig = createEvmChainConfig({
+  cron: '*/30 * * * *',
+  maxDuration: 1000,
+  network: 'abstract',
+  chain: 'abstract',
+  facilitators: FACILITATORS_BY_CHAIN(Chain.ABSTRACT),
+});
